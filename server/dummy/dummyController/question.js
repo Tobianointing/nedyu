1f2be7a042c4289390878a5ee72ad3583603fd16
nedyudombat <nedyudombat@gmail.com>
2019-01-04 11:54:54 +0100

    [ft #162975713]refactor git status

--- server/dummy/dummyController/question.js
+++ server/dummy/dummyController/question.js
@@ -9,20 +9,12 @@ let downcount = 1;
 class QuestionController {
   static createQuestion(req, res) {
     const { user, meetup, title, body } = req.body;
-
-    const fields = {
-      user,
-      meetup,
-      title,
-      body,
-    };
+    const fields = { user, meetup, title, body };
     const validator = new Validator();
     validator.validate(fields, 'required|string');
-
     if (!validator.hasErrors) {
       const foundUsername = dummyUser.find(user => user.username === req.body.user);
       const foundMeetup = dummyMeetup.find(meetup => meetup.topic === req.body.meetup);
-
       if (foundUsername && foundMeetup) {
         const id = dummyQuestion.length + 1;
         const userId = foundUsername.id;
@@ -35,36 +27,19 @@ class QuestionController {
           return res.status(409).json({ error: `This question '${fields.title}' has already been asked for this meetup` });
         }
         const votes = 0;
-        const questionDetail = {
-          id,
-          user,
-          meetup,
-          title,
-          body,
-          votes,
-          createdOn: new Date(),
-        };
+        const questionDetail = { id, user, meetup, title, body, votes, createdOn: new Date() };
         dummyQuestion.push(questionDetail);
-        const resDetails = {
-          userId,
-          meetupId,
-          title,
-          body,
-        }
+        const resDetails = { userId, meetupId, title, body }
         return res.status(201).json({
           status: 201,
           message: 'Question asked successfully',
           data: resDetails,
         });
       } else {
-        return res.status(404).json({
-          error: 'User or Meetup does not exist',
-        });
+        return res.status(404).json({ error: 'User or Meetup does not exist' });
       }
     } else {
-      return res.status(400).json({
-        errorMessages:validator.getErrors()
-      });
+      return res.status(400).json({ errorMessages:validator.getErrors() });
     }
   }
 }
