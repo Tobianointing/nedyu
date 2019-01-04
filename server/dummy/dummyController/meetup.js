abccb85ea5e27b2c0100492338357ec10eec83ca
nedyudombat <nedyudombat@gmail.com>
2019-01-04 12:15:59 +0100

    [ft #162975752]Setup questioner endpoint to GET a specific Meetup

--- server/dummy/dummyController/meetup.js
+++ server/dummy/dummyController/meetup.js
@@ -3,6 +3,23 @@ import dummyUser from '../dummyModel/dummyUser';
 import Validator from '../_helpers/post_validators';
 
 class MeetupController {
+
+  static getSingleMeetup(req, res) {
+    const foundMeetup = dummyMeetup.find(meetup => meetup.id === parseInt(req.params.id, 10));
+
+    if (foundMeetup) {
+      return res.status(200).json({
+        status: 200,
+        message: 'Successfully retrieved specific meetup',
+        data: foundMeetup,
+      });
+    }
+    return res.status(404).json({
+      status: 404,
+      error: 'Meetup record not found',
+    });
+  }
+
   static createMeetup(req, res) {
     const { organizer, topic, happeningOn, location, tags, images, username } = req.body;
     const fields = { organizer, topic, happeningOn, location, username };
